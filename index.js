4c157e0a1510d6f2e0f5eadbfadf6c55f3b96c88
Marc Maldonado <marc@telecomelite.com>
2020-02-15 15:43:25 -0500

    getting started

---
+++ index.js
@@ -0,0 +1,57 @@
+const fs = require("fs")
+const axios = require("axios")
+const inquirer = require("inquirer")
+const pdf = require("html-pdf")
+const open = require("open")
+const generateHTML = require("./generateHTML")
+
+
+
+const data = {}
+const questions = [{
+    type: "input",
+    name: "username",
+    message: "Enter your GitHub username: ",
+},
+{
+    type: "list",
+    name: "color",
+    message: "What is your favorite color?",
+    choices: ["green", "blue", "pink", "red"],
+}];
+
+init();
+function init() {
+    inquirer
+        .prompt(questions)
+        .then(function ({ username, color }) {
+            const queryURL = `https://api.github.com/users/${username}`;
+            const querlURLStar = `https://api.github.com/users/${username}/starred`
+            axios.get(queryURL).
+                then(function ({ data }) {
+                    axios.get(querlURLStar)
+                        .then(function (res) {
+                            const sCount = res.data.map(element => {
+                                return element.stargazers_count
+                            });
+                            const stars = sCount.length;
+                            const params = {
+                                color: color,
+                                username: data.username,
+                                avatar_url: data.avatar_url,
+                                name: data.name,
+                                location: data.location,
+                                bio: data.bio,
+                                public_repos: data.public_repos,
+                                followers: data.followers,
+                                following: data.following,
+                                github: data.login,
+                                stars: stars,
+                            }
+                            pdf.create(generateHTML(params)).toFile('./devportfolio.pdf', function (err, res) {
+                                if (err) throw (err);
+                            })
+                        })
+                })
+        })
+}
